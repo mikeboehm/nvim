8888b5efb303f0a891530de3d78550660ff357c4
Mike Boehm <mike@boehm.com.au>
2025-03-07 11:50:49 +1100

    set leader key, add which-key and todo-comments

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,12 @@
+return {
+{
+   "folke/which-key.nvim",
+   enabled = true,
+   opts = {
+     preset = "helix",
+     debug = vim.uv.cwd():find("which%-key"),
+     win = {},
+     spec = {},
+   },
+ }}
+
