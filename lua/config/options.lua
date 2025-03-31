1d69be323778edbfdb393ec15c21b2256310926e
Mike Boehm <mike@myzeller.com>
2025-04-01 10:48:32 +1100

    relative line numbers except for current line

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,4 +1,9 @@
+-- set absolute line number for current line
+vim.opt.number = true
+-- show relative line numbers for all other lines
+vim.opt.relativenumber = true
+vim.cmd("set cursorline")
+vim.cmd("set cursorlineopt=number")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set tabstop=2")
-vim.opt.relativenumber = true
