cbcdb3e4c056b4664567fe6b917d407b0e71fbff
Mike Boehm <mike@myzeller.com>
2025-04-01 16:40:27 +1100

    new theme tokyonight

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+	{
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+		style = "night",
+	},
+	config = function()
+    -- load the colorscheme here
+    vim.cmd([[colorscheme tokyonight]])
+	end,
+},
+}
+
