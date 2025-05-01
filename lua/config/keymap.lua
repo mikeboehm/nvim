1662dea64bef1e5c78b53579d4d7848bed2dc124
Mike Boehm <mike@boehm.com.au>
2025-05-01 21:41:51 +1000

    added "Save" and buffer navigation keymaps

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -7,6 +7,10 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = 'Yank to clipboard' })
 vim.keymap.set({ "n", "v" }, "<leader>p", '"+p', { desc = 'Paste from clipboard' })
 
+vim.keymap.set({ "n", "v" }, "<leader>s", ':w<cr>', { desc = 'Save' })
+vim.keymap.set({ "n", "v" }, "<leader>e", ':bprevious<cr>', { desc = 'Previous Buffer' })
+vim.keymap.set({ "n", "v" }, "<leader>i", ':bnext<cr>', { desc = 'Next Buffer' })
+vim.keymap.set({ "n", "v" }, "<leader>R", ':luafile %<cr>', { desc = 'Source current lua file' })
 -- cursor up/down 5 lines
 vim.keymap.set("", "<C-j>", "5j")
 vim.keymap.set("", "<C-k>", "5k")
