937a163561f3df3c8f8751c4b8aceb8c94e2590e
Mike Boehm <mike@boehm.com.au>
2025-05-01 21:42:29 +1000

    added mini.nvim and web dev icons

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -2,12 +2,14 @@
 
 return {
 	{
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
-}
+	  "folke/todo-comments.nvim",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		opts = {
+			-- your configuration comes here
+			-- or leave it empty to use the default settings
+			-- refer to the configuration section below
+		}
+	},
+	{ "nvim-tree/nvim-web-devicons", opts = {} },
+  { 'echasnovski/mini.nvim', version = false }
 }
